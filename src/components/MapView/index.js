87e4769d322515ef9bc2f5edfce12efb913bc34e
Thomas Chia <thomas.chia@amaas.com>
2018-01-07 17:46:35 +0800

    Add CountryDetails component

--- src/components/MapView/index.js
+++ src/components/MapView/index.js
@@ -5,13 +5,41 @@ import {
   withGoogleMap,
   withScriptjs
 } from 'react-google-maps'
+import { InfoBox } from 'react-google-maps/lib/components/addons/InfoBox'
 
-const MapView = () => {
-  return (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 1.3521, lng: 103.8198 }}>
-      <Marker position={{ lat: 1.3521, lng: 103.8198 }} />
-    </GoogleMap>
-  )
+import CountryDetails from '../CountryDetails'
+
+class MapView extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      detailVisible: false,
+      lat: 1.3521,
+      lng: 103.8198
+    }
+    this.toggleDetailVisible = this.toggleDetailVisible.bind(this)
+  }
+
+  toggleDetailVisible() {
+    this.setState({ detailVisible: !this.state.detailVisible })
+  }
+
+  render() {
+    const { state: { detailVisible, lat, lng } } = this
+    return (
+      <GoogleMap defaultZoom={8} defaultCenter={{ lat, lng }}>
+        <Marker position={{ lat, lng }} onClick={this.toggleDetailVisible}>
+          {detailVisible ? (
+            <InfoBox
+              options={{ closeBoxURL: ``, enableEventPropagation: true }}
+            >
+              <CountryDetails />
+            </InfoBox>
+          ) : null}
+        </Marker>
+      </GoogleMap>
+    )
+  }
 }
 
 export default withScriptjs(withGoogleMap(MapView))
